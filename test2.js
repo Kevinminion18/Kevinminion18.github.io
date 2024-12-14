b3890ab6262eb59910d6fedb5aa075056ba703ba
kshao918 <kshao918@uw.edu>
2024-12-14 10:59:59 -0800

    update

--- test2.js
+++ test2.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", function() {
       sections.forEach((section) => {
           const sectionTop = section.offsetTop;
           const sectionHeight = section.clientHeight;
-          if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+          if (scrollY >= (sectionTop - sectionHeight / 3)) {
               current = section.getAttribute('id');
           }
       });
