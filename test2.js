c1fdc6c7359ae384f70cc2af0c77303def89b7e2
kshao918 <kshao918@uw.edu>
2024-12-14 10:22:21 -0800

    test-js

---
+++ test2.js
@@ -0,0 +1,36 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+  const sections = document.querySelectorAll("section");
+  const navLinks = document.querySelectorAll("nav ul li a");
+
+  function setActiveLink(section) {
+      navLinks.forEach(link => {
+          link.classList.toggle('active', '#' + section.id === link.getAttribute('href'));
+      });
+  }
+
+  window.addEventListener('scroll', () => {
+      let current = "";
+      sections.forEach(section => {
+          const sectionTop = section.offsetTop;
+          const sectionHeight = section.clientHeight;
+          if (pageYOffset >= (sectionTop - sectionHeight / 3)) {
+              current = section.getAttribute('id');
+          }
+      });
+      setActiveLink(document.getElementById(current));
+  });
+
+
+  navLinks.forEach(link => {
+      link.addEventListener('click', function(e) {
+          e.preventDefault();
+          const targetId = this.getAttribute('href').substring(1);
+          const targetSection = document.getElementById(targetId);
+          window.scrollTo({
+              top: targetSection.offsetTop,
+              behavior: 'smooth'
+          });
+      });
+  });
+});
